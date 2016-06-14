389a0db605736d81c41ce8c2f827c8469dbdfe2c
Jonathan <jonacom@lissismore.com>
2016-06-14 10:49:00 -0600

    still having issues

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,13 +1,11 @@
 class BestBuyService
   def initialize
     @conn = Faraday.new(url: "https://api.bestbuy.com/v1")
-    # @conn.headers["apiKey"] = ENV["KEY"]
   end
 
   def get_stores(zip)
-    require "pry"; binding.pry
-    parsed = parse(@conn.get(URI.encode("/stores(area(#{zip}, 25))?apiKey=#{ENV["KEY"].dup}")))
-    require "pry"; binding.pry
+    key = ENV["KEY"].dup
+    @conn.get("/stores(area(#{zip},25))"), { apiKey: key, format: "json" }
   end
 
   def parse(input)
