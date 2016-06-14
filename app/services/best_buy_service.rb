56af05e711f1e53571cd1e1e0f67d434960fd87b
Jonathan <jonacom@lissismore.com>
2016-06-14 10:30:53 -0600

    having problems with NOT FOUND being my response :(

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,10 +1,16 @@
 class BestBuyService
   def initialize
-    @conn = Faraday.new("https://api.bestbuy.com/v1/stores")
-    @conn.headers["apiKey"] = ENV["KEY"]
+    @conn = Faraday.new(url: "https://api.bestbuy.com/v1")
+    # @conn.headers["apiKey"] = ENV["KEY"]
   end
 
   def get_stores(zip)
-    @conn.get("(area(#{zip}, 25))")
+    require "pry"; binding.pry
+    parsed = parse(@conn.get(URI.encode("/stores(area(#{zip}, 25))?apiKey=#{ENV["KEY"].dup}")))
+    require "pry"; binding.pry
+  end
+
+  def parse(input)
+    JSON.parse(input.body)
   end
 end
